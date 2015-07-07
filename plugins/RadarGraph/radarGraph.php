37bc260825855b62781f829700ea8933682543b9
Merel <merel@alexander.khleuven.be>
2015-07-07 19:17:14 +0200

    PHP Object model based on JSON created for graph creation. Graph is rendered to file and saved in tmp folder.
    LimeSurvey/plugins/RadarGraph/radarGraph.php?testimage=j creates random radar plot with 3 categories
    
    TODO: use YUI library loader

--- plugins/RadarGraph/radarGraph.php
+++ plugins/RadarGraph/radarGraph.php
@@ -3,36 +3,53 @@
  // Standard inclusions     
 
 
+/**
+*  Generates a graph in tmp folder based on assessment data and returns unique filename.
+*
+*  Example input: browse to plugins/RadarGraph/radarGraph.php?testimage=j
+**/
 function generateRadarGraph($assessments) {
- 
+
+
+	// TODO use YUI framework to load resources correctly 
         require_once('/var/www/pwo-vadvies/limesurvey/application/third_party/pchart/pchart/pChart.class');
 	require_once('/var/www/pwo-vadvies/limesurvey/application/third_party/pchart/pchart/pData.class');
 	require_once('/var/www/pwo-vadvies/limesurvey/application/third_party/pchart/pchart/pCache.class');
 
-// TODO somewhere else!
-//$content = "<h2>Summary Radar graph</h2>";
-
 // Dataset definition   
  $DataSet = new pData;  
- $DataSet->AddPoint(array("Kennisverwerven","Gezondheidsbevordering","Permanente Evaluatie","Consultatie","Promotie", "Praktijkchecklist"),"Label");  
- $DataSet->AddPoint(array(rand(1,10),rand(1,10),rand(1,10),rand(1,10),rand(1,10),rand(1,10)),"Serie1");  
- $DataSet->AddPoint(array(rand(1,10),rand(1,10),rand(1,10),rand(1,10),rand(1,10),rand(1,10)),"Serie2");  
- $DataSet->AddSerie("Serie1");  
- $DataSet->AddSerie("Serie2");  
- $DataSet->SetAbsciseLabelSerie("Label");  
-  
-  
- $DataSet->SetSerieName("Referentie","Serie1");  
- $DataSet->SetSerieName("Uw competenties","Serie2");  
-  
- // Initialise the graph  
- $Test = new pChart(600,500);  
- $Test->setFontProperties("/var/www/pwo-vadvies/limesurvey/fonts/DejaVuSans.ttf",8);  
- //$Test->drawFilledRoundedRectangle(7,7,393,393,5,240,240,240); 
- //$Test->drawRoundedRectangle(5,5,395,395,5,230,230,230);  
+
+ $aAxisses = array();
+ $aSerie1 = array();
+ $aSerie2 = array();
+ foreach ($assessments->assessmentgroup as $category) {
+	$aAxisses[] = $category->title;
+	$aSerie1[] = $category->score;
+	$aSerie2[] = $category->ref;
+ }
+
+// $DataSet->AddPoint(array("Kennisverwerven","Gezondheidsbevordering","Permanente Evaluatie","Consultatie","Promotie"),"Label");
+// $DataSet->AddPoint(array(rand(1,10),rand(1,10),rand(1,10),rand(1,10),rand(1,10)),"Serie1");
+// $DataSet->AddPoint(array(rand(1,10),rand(1,10),rand(1,10),rand(1,10),rand(1,10)),"Serie2");
+
+ $DataSet->AddPoint($aAxisses,"Label");
+$DataSet->AddPoint($aSerie1,"Serie1");
+$DataSet->AddPoint($aSerie2,"Serie2");
+
+ $DataSet->AddSerie("Serie1");
+ $DataSet->AddSerie("Serie2");
+ $DataSet->SetAbsciseLabelSerie("Label");
+
+
+ $DataSet->SetSerieName("Referentie","Serie1");
+ $DataSet->SetSerieName("Uw competenties","Serie2");
+
+
+// Initialise the graph
+ $Test = new pChart(600,500);
+ // TODO localization for fonts needed
+ $Test->setFontProperties("/var/www/pwo-vadvies/limesurvey-devop/LimeSurvey/fonts/DejaVuSans.ttf",8);  
  $Test->setGraphArea(80,30,450,450);  
- //$Test->drawFilledRoundedRectangle(30,30,370,370,5,255,255,255);  
- //$Test->drawRoundedRectangle(30,30,370,370,5,220,220,220);  
   
  // Draw the radar graph  
  $Test->drawRadarAxis($DataSet->GetData(),$DataSet->GetDataDescription(),TRUE,20,120,120,120,230,230,230);  
@@ -42,12 +59,24 @@ function generateRadarGraph($assessments) {
  $Test->drawLegend(15,15,$DataSet->GetDataDescription(),255,255,255);  
  $Test->setFontProperties("Fonts/tahoma.ttf",10);  
  $Test->drawTitle(0,22,"Example 8",50,50,50,400);  
-// echo "ok";
-// FAILS $Test->Render("example8.png");
-// echo "<img src='example8.png' />";
- $Test->Stroke();
+ 
+  // Save to tmp folder to embed in html and pdf report
+  $sRenderedFilename = $assessments->surveyid . "-" . uniqid() . ".png";
+
+  // TODO localization of tmp folder
+  $Test->Render("/var/www/pwo-vadvies/limesurvey-devop/LimeSurvey/tmp/upload/".$sRenderedFilename);
+
+  return $sRenderedFilename;
 }
 
+    if(isset($_GET["testimage"])) {
+	// Sample survey with 3 assessment groups
+    	$t_json = '{ "surveyid": "1258", "assessmentgroup": [ { "groupname": "Example 1", "title": "Example 1 assessment", "min": 0, "max": 10, "score": 5, "ref": '.rand(1,10).' }, { "groupname": "Example 2", "title": "Example 2 assessment", "min": 0, "max": 10, "score": 3, "ref": '.rand(1,10).' }, { "groupname": "Example 3", "title": "Example 3 assessment", "min": 0, "max": 10, "score": 7, "ref": '.rand(1,10).' } ] }';
+	
+	echo "Example input: ".$t_json . "<br />";
+	
+    	$demo_assessment = json_decode($t_json);
 
-    generateRadarGraph(null);
+    	echo "Filename: <a href='http://alexander.khleuven.be/pwo-vadvies/limesurvey-devop/LimeSurvey/tmp/upload/".generateRadarGraph($demo_assessment)."'>Link to file</a>";
+    } else { /*echo ""; // TESTING PURPOSES */ } 
 ?>
